f54a37fd5c8aad87ae6a3a0633e261a6c3fee6f8
suhanolodha27 <72444550+suhanolodha27@users.noreply.github.com>
2020-10-31 19:43:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,39 @@
-var fixedRect, movingRect;
+const Engine=Matter.Engine; 
+const World=Matter.World; 
+const Bodies=Matter.Bodies;
+var engine,world;
+var wall,car; 
+var speed,weight;
+function setup(){ 
+var canvas = createCanvas(400,400); 
+engine=Engine.create(); 
+world=engine.world; 
 
-function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
+speed=random(55,90);
+weight=random(400,150);
+
+var wall_options={restitution:1.0}
+wall=Bodies.rectangle(100,200,300,200,wall_options);
+World.add(world,wall);
+
+car=createSprite(50,200,50,50);
+wall=createSprite(300,200,60,height/2);
+World.add(world,car);} 
+function draw(){ 
+background(220);
+Engine.update(engine);
+car.velocityX=speed;
+
+if(wall.x-car.<(car.width+wall.width)/2){
+    car.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    if(deformation>180{
+   car.shapeColor(255,0,0) ;  } 
+   if(deformation<180 && deformation>100){
+    car.shapeColor=colo(230,230,0); }
+    if(deformation<180{
+        car.shapeColor(0,255,0) ;  }  
+   
+    )
 }
-
-function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    fixedRect.shapeColor = "green";
-  }
-  drawSprites();
 }
