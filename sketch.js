e15c636137506be3c6cc5e11a31db65773650130
suhanolodha27 <72444550+suhanolodha27@users.noreply.github.com>
2020-11-02 13:44:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,11 +27,11 @@ car.velocityX=speed;
 if(wall.x-car.<(car.width+wall.width)/2){
     car.velocityX=0;
     var deformation=0.5*weight*speed*speed/22500;
-    if(deformation>180{
+    if(deformation)>180{
    car.shapeColor(255,0,0) ;  } 
    if(deformation<180 && deformation>100){
     car.shapeColor=colo(230,230,0); }
-    if(deformation<180{
+    if(deformation<180){
         car.shapeColor(0,255,0) ;  }  
    
     )
